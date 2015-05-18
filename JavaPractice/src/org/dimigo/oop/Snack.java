aa37158ac5a1612b403f0a2f96d83e7f624ebb9e
박유택 <parkyutack12@naver.com>
2015-05-18 11:41:32 +0900

    치-즈 볼

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ *<pre>
+ *org.dimigo.oop
+ * |_Snack
+ *1. 개요:
+ *2. 작성일: 2015. 5. 18.
+ *</pre>
+ * @author : 박유택
+ * @version : 1.0
+ */
+public class Snack {
+
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+	
+
+	public Snack() {};
+	public Snack(String name, String company, int price, int number) {
+		this.name = name;
+		this.company = company;
+		this.price = price;
+		this.number = number;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void printSnack() {
+		System.out.println("이름 : " + name);
+		System.out.println("제조사 : " + company);
+		System.out.println("가격 : " + String.format("%,d", price) + "원");
+		System.out.println("개수 : " + number + "개");
+		System.out.println();
+	}
+	public int calcPrice(){
+		return price * number;
+	}
+
+}
