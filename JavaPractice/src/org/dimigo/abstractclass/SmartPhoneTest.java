cb6f2771bd8c4f957c29e8a96bb2c46944932ec3
박유택 <parkyutack12@naver.com>
2015-09-01 10:16:50 +0900

    a

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ *<pre>
+ *org.dimigo.inheritance
+ * |_SmartPhoneTest
+ *1. 개요:
+ *2. 작성일: 2015. 8. 25.
+ *</pre>
+ * @author : 박유택
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SmartPhone myPhone = new IPhone("iPhone6", "애플", 700000);
+		SmartPhone yourPhone = new Galaxy("갤럭시 S6", "삼성", 650000);
+		
+		System.out.println(myPhone.toString());
+		myPhone.turnOn();
+		myPhone.pay();
+		myPhone.useSpecialFunction();
+		myPhone.turnOff();
+		
+		System.out.println();
+		
+		System.out.println(yourPhone.toString());
+		yourPhone.turnOn();
+		yourPhone.pay();
+		yourPhone.useSpecialFunction();
+		yourPhone.turnOff();
+	}
+
+}
